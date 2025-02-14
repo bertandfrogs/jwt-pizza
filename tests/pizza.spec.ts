b9a94f2ed2cf2ac2537293dbd1a5a7a5e743a719
Sarah Smalley <bertandfrogs@gmail.com>
2025-02-13 18:37:35 -0700

    up to 88% coverage now

--- tests/pizza.spec.ts
+++ tests/pizza.spec.ts
@@ -6,6 +6,7 @@ const adminPass = 'admin';
 const franchEmail = 'b@jwt.com';
 const franchPass = '4RGk34Uk2y9JPxH';
 
+// Public Pages (not logged in)
 test('home page is displayed correctly', async ({ page }) => {
 	await page.goto("/");
 	expect(await page.title()).toBe('JWT Pizza');
@@ -26,6 +27,29 @@ test('logged out user cannot order pizzas', async ({ page }) => {
 	await expect(page.getByRole('textbox', { name: 'Email address' })).toBeVisible();
 });
 
+test('franchise non-franchisee', async ({ page }) => {
+	await page.goto('/');
+	await page.getByLabel('Global').getByRole('link', { name: 'Franchise' }).click();
+	await expect(page.getByRole('list')).toContainText('homefranchise-dashboard');
+	await expect(page.getByRole('main')).toContainText('So you want a piece of the pie?');
+	await expect(page.getByRole('alert')).toBeVisible();
+})
+
+
+test('about page', async ({ page }) => {
+	await page.goto('/');
+	await page.getByRole('link', { name: 'About' }).click();
+	await expect(page.getByRole('list')).toContainText('homeabout');
+	await expect(page.getByRole('main')).toContainText('The secret sauce');
+});
+
+test('history page', async ({ page }) => {
+	await page.goto('/');
+	await page.getByRole('link', { name: 'History' }).click();
+	await expect(page.getByRole('list')).toContainText('homehistory');
+	await expect(page.getByRole('heading')).toContainText('Mama Rucci, my my');
+})
+
 test('log in with invalid credentials', async ({ page }) => {
 	await page.goto("/");
 	await page.getByRole('link', { name: 'Login' }).click();
@@ -36,13 +60,20 @@ test('log in with invalid credentials', async ({ page }) => {
 	await expect(page.getByRole('main')).toContainText('{"code":404,"message":"unknown user"}');
 })
 
+test('404 page', async ({page}) => {
+	await page.goto('/jwtsux');
+	await expect(page.getByRole('heading')).toContainText('Oops');
+ 	await expect(page.getByRole('main')).toContainText('It looks like we have dropped a pizza on the floor. Please try another page.');
+})
+
 test('successful login and logout display correctly', async ({ page }) => {
 	// login
 	page = await login(page, adminEmail, adminPass);
 
 	// logout
 	await expect(page.locator('#navbar-dark')).toContainText('Logout');
-	await page.getByRole('link', { name: 'Logout' }).click();
+	await logout(page);
+	
 	await expect(page.locator('#navbar-dark')).toContainText('Login');
 });
 
@@ -54,6 +85,8 @@ test('diner dashboard', async ({ page }) => {
 	await expect(page.getByRole('heading')).toContainText('Your pizza kitchen');
 	await expect(page.getByRole('main')).toContainText('常用名字');
 	await expect(page.getByRole('main')).toContainText('a@jwt.com');
+
+	await logout(page);
 });
 
 test('order a pizza', async ({ page }) => {
@@ -95,23 +128,12 @@ test('order a pizza', async ({ page }) => {
 	await page.getByRole('button', { name: 'Order more' }).click();
 	await expect(page.getByRole('list')).toContainText('homemenu');
 	await expect(page.locator('form')).toContainText('What are you waiting for? Pick a store and then add some pizzas!');
-});
 
-test('franchise non-franchisee', async ({ page }) => {
-	await page.goto('/');
-	await page.getByLabel('Global').getByRole('link', { name: 'Franchise' }).click();
-	await expect(page.getByRole('list')).toContainText('homefranchise-dashboard');
-	await expect(page.getByRole('main')).toContainText('So you want a piece of the pie?');
-	await expect(page.getByRole('alert')).toBeVisible();
-})
+	await logout(page);
+});
 
 test('franchise valid franchisee', async ({ page }) => {
 	page = await login(page, franchEmail, franchPass);
-
-	await page.goto("/");
-
-	// make sure the page has navigated home and loaded the dashboard icon
-	await expect(page.locator('#diner-dashboard')).toBeVisible({ timeout: 10000 });
 
 	await page.getByLabel('Global').getByRole('link', { name: 'Franchise' }).click();
 	await expect(page.getByRole('list')).toContainText('homefranchise-dashboard');
@@ -119,23 +141,62 @@ test('franchise valid franchisee', async ({ page }) => {
 	await expect(page.getByRole('button', { name: 'Create store' })).toBeVisible();
 	await page.getByRole('button', { name: 'Create store' }).click();
 	await expect(page.getByText('Create store')).toBeVisible();
+
+	await logout(page);
 });
 
-test('about page', async ({ page }) => {
-	await page.goto('/');
-	await page.getByRole('link', { name: 'About' }).click();
-	await expect(page.getByRole('list')).toContainText('homeabout');
-	await expect(page.getByRole('main')).toContainText('The secret sauce');
-});
+test('admin dashboard', async ({ page }) => {
+	page = await login(page, adminEmail, adminPass);
+	await page.getByRole('link', { name: 'Admin' }).click();
+	await page.locator('#root div').filter({ hasText: 'Keep the dough rolling and' }).nth(3).click();
+	await expect(page.getByRole('main')).toContainText('Keep the dough rolling and the franchises signing up.');
+	await expect(page.getByRole('button', { name: 'Add Franchise' })).toBeVisible();
+	await page.getByRole('button', { name: 'Add Franchise' }).click();
+	await page.getByRole('textbox', { name: 'franchise name' }).click();
+	await page.getByRole('textbox', { name: 'franchise name' }).fill('placeholder');
+	await page.getByRole('textbox', { name: 'franchisee admin email' }).click();
+	await page.getByRole('textbox', { name: 'franchisee admin email' }).fill(franchEmail);
+	
+	await page.getByRole('button', { name: 'Create' }).click();
+	await expect(page.getByRole('table')).toContainText('placeholder');
+	await page.getByRole('row', { name: 'placeholder b Close' }).getByRole('button').click();
+	await expect(page.getByRole('main')).toContainText('Are you sure you want to close the placeholder franchise? This will close all associated stores and cannot be restored. All outstanding revenue will not be refunded.');
+	
+	await page.getByRole('button', { name: 'Close' }).click();
+	
+	await expect(page.getByRole('table')).not.toContainText('placeholder');
 
-test('history page', async ({ page }) => {
-	await page.goto('/');
-	await page.getByRole('link', { name: 'History' }).click();
-	await expect(page.getByRole('list')).toContainText('homehistory');
-	await expect(page.getByRole('heading')).toContainText('Mama Rucci, my my');
+	await logout(page);
 })
 
-// helper function
+test('register', async ({ page }) => {
+	const testName = Math.random().toString(36).substring(2, 12);
+	const testEmail = testName + "@jwt.com";
+	const testPass = Math.random().toString(36).substring(2, 12);
+
+	await page.goto('/');
+	await page.getByRole('link', { name: 'Register' }).click();
+	await expect(page.getByRole('heading')).toContainText('Welcome to the party');
+	await page.getByRole('textbox', { name: 'Full name' }).click();
+	await page.getByRole('textbox', { name: 'Full name' }).fill(testName);
+	await page.getByRole('textbox', { name: 'Email address' }).click();
+	await page.getByRole('textbox', { name: 'Email address' }).fill(testEmail);
+	await page.getByRole('textbox', { name: 'Password' }).click();
+	await page.getByRole('textbox', { name: 'Password' }).fill(testPass);
+	const registerResponse = page.waitForResponse('http://localhost:3000/api/auth');
+	await page.getByRole('button', { name: 'Register' }).click();
+	await registerResponse;
+
+	await page.locator('#diner-dashboard').click();
+	await expect(page.getByRole('heading')).toContainText('Your pizza kitchen');
+	await expect(page.getByRole('main')).toContainText(testName);
+	await expect(page.getByRole('main')).toContainText(testEmail);
+	await expect(page.getByRole('main')).toContainText('diner');
+
+	await logout(page);
+});
+
+// helper functions
 async function login(page: Page, email: string, password: string) {
 	await page.goto('/');
 	await page.getByRole('link', { name: 'Login' }).click();
@@ -147,4 +208,10 @@ async function login(page: Page, email: string, password: string) {
 	await page.getByRole('button', { name: 'Login' }).click();
 	await loginResponse;
 	return page;
+}
+
+async function logout(page: Page) {
+	const logoutResponse = page.waitForResponse("http://localhost:3000/api/auth");
+	await page.getByRole('link', { name: 'Logout' }).click();
+	await logoutResponse;
 }
